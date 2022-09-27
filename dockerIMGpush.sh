18a5c44db684d37b4ad257ec38b7b2f8da3eb4a0
Rajani Ekunde <43741838+rajani103@users.noreply.github.com>
2022-09-27 20:19:36 +0530

    Update dockerIMGpush.sh

--- dockerIMGpush.sh
+++ dockerIMGpush.sh
@@ -1,5 +1,5 @@
-REG_USERNAME="rajan9886886836"
-REG_PASSWORD="R@j@n5707"
+REG_USERNAME="rajjo103"
+REG_PASSWORD="Bhaubhau@103"
 
 echo "====> Logging into conatiner"
 sudo docker login -u $REG_USERNAME -p $REG_PASSWORD
@@ -8,19 +8,19 @@ if [ $? -ne 0 ] ; then
         exit 1
 fi
 echo "====> Tagging the image"
-docker tag api-gateway rajan9886886836/dockertry:api-gateway
-docker tag mysql rajan9886886836/dockertry:mysql
-docker tag build-bookings-management rajan9886886836/dockertry:build-bookings-management
-docker tag build-email-service rajan9886886836/dockertry:build-email-service
-docker tag build-chat-service rajan9886886836/dockertry:build-chat-service
-docker tag build-payment-service rajan9886886836/dockertry:build-payment-service
-docker tag build-user-service rajan9886886836/dockertry:build-user-service
-docker tag build-authentication-service rajan9886886836/dockertry:build-authentication-service
-docker tag build-eureka-server rajan9886886836/dockertry:build-eureka-server
-docker tag build-eureka-server rajan9886886836/dockertry:build-product-webapp
-docker tag rabbitmq:management rajan9886886836/dockertry:rabbitmq
-docker tag mongo rajan9886886836/dockertry:mongo
-docker tag config rajan9886886836/dockertry:config
+docker tag api-gateway rajjo103/capstone:api-gateway
+docker tag mysql rajjo103/capstone:mysql
+docker tag build-bookings-management rajjo103/capstone:build-bookings-management
+docker tag build-email-service rajjo103/capstone:build-email-service
+docker tag build-chat-service rajjo103/capstone:build-chat-service
+docker tag build-payment-service rajjo103/capstone:build-payment-service
+docker tag build-user-service rajjo103/capstone:build-user-service
+docker tag build-authentication-service rajjo103/capstone:build-authentication-service
+docker tag build-eureka-server rajjo103/capstone:build-eureka-server
+docker tag build-eureka-server rajjo103/capstone:build-product-webapp
+docker tag rabbitmq:management rajjo103/capstone:rabbitmq
+docker tag mongo rajjo103/capstone:mongo
+docker tag config rajjo103/capstone:config
 
 if [ $? -ne 0 ] ; then
         echo "ERROR: Docker tagging images fialed"
@@ -29,19 +29,19 @@ fi
 
 
 echo "====> Pushing the image to docker repositoy"
-docker push  rajan9886886836/dockertry:api-gateway
-docker push  rajan9886886836/dockertry:mysql
-docker push  rajan9886886836/dockertry:build-bookings-management
-docker push  rajan9886886836/dockertry:build-email-service
-docker push  rajan9886886836/dockertry:build-chat-service
-docker push  rajan9886886836/dockertry:build-payment-service
-docker push  rajan9886886836/dockertry:build-user-service
-docker push  rajan9886886836/dockertry:build-authentication-service
-docker push  rajan9886886836/dockertry:build-eureka-server
-docker push  rajan9886886836/dockertry:build-product-webapp
-docker push  rajan9886886836/dockertry:rabbitmq
-docker push  rajan9886886836/dockertry:mongo
-docker push  rajan9886886836/dockertry:config
+docker push  rajjo103/capstone:api-gateway
+docker push  rajjo103/capstone:mysql
+docker push  rajjo103/capstone:build-bookings-management
+docker push  rajjo103/capstone:build-email-service
+docker push  rajjo103/capstone:build-chat-service
+docker push  rajjo103/capstone:build-payment-service
+docker push  rajjo103/capstone:build-user-service
+docker push  rajjo103/capstone:build-authentication-service
+docker push  rajjo103/capstone:build-eureka-server
+docker push  rajjo103/capstone:build-product-webapp
+docker push  rajjo103/capstone:rabbitmq
+docker push  rajjo103/capstone:mongo
+docker push  rajjo103/capstone:config
 
 if [ $? -ne 0 ] ; then
         echo "ERROR: Docker Pushing the images failed"
