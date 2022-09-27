c3f10c44071b86d2df242b72a6a259fc0e95353a
Rajani Ekunde <43741838+rajani103@users.noreply.github.com>
2022-09-27 20:09:36 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
-REG_USERNAME="rajan9886886836"
-REG_PASSWORD="R@j@n5707"
+REG_USERNAME="rajjo103"
+REG_PASSWORD="Bhaubhau@103"
 echo "====> Logging into conatiner"
 sudo docker login -u $REG_USERNAME -p $REG_PASSWORD
 if [ $? -ne 0 ] ; then
@@ -7,13 +7,13 @@ if [ $? -ne 0 ] ; then
         exit 1
 fi
 
-#sudo docker tag mongo:mongo1 rajan9886886836/dockertry:mongo1
-sudo docker tag config:config1 rajan9886886836/dockertry:config1
-sudo docker tag mysql:mysql rajan9886886836/dockertry:mysql
+#sudo docker tag mongo:mongo1 rajjo103/capstone:mongo1
+sudo docker tag config:config1 rajjo103/capstone:config1
+sudo docker tag mysql:mysql rajjo103/capstone:mysql
 echo "====> Inintialising Pushing the image to conatiner"
-#sudo docker push rajan9886886836/dockertry:mongo1
-sudo docker push rajan9886886836/dockertry:config1
-sudo docker push rajan9886886836/dockertry:mysql
+#sudo docker push rajjo103/capstone:mongo1
+sudo docker push rajjo103/capstone:config1
+sudo docker push rajjo103/capstone:mysql
 if [ $? -ne 0 ] ; then
         echo "ERROR: Docker PUSH failed"
         exit 1
